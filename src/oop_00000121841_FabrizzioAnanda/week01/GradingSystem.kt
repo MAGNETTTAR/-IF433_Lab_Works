06276dcf3a291a48d721fb5cf0ba54ab3003f258
FabrizzioAnanda <fabrizzio.ananda@student.umn.ac.id>
2026-02-08 02:21:53 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000121841_FabrizzioAnanda/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000121841_FabrizzioAnanda.week01
+
+fun main (){
+    var name: String = "Fabrizzio Ananda"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
