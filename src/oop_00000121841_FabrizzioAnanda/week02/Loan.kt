27a38ab7dcbc9764a8d3c08833973624e63bd9b3
FabrizzioAnanda <fabrizzio.ananda@student.umn.ac.id>
2026-02-11 15:51:08 +0700

    week02: (task 1) implement library input system

--- src/oop_00000121841_FabrizzioAnanda/week02/Loan.kt
+++ src/oop_00000121841_FabrizzioAnanda/week02/Loan.kt
@@ -1,5 +1,7 @@
 package oop_00000121841_FabrizzioAnanda.week02
 
+import java.util.Scanner
+
 class Loan(
     val bookTitle: String,
     val borrower: String,
@@ -12,4 +14,31 @@ class Loan(
             0
         }
     }
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- LIBRARY FINE SYSTEM ---")
+
+    print("Masukkan Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Masukkan Lama Pinjam (Hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) {
+        duration = 1
+    }
+
+    val loanRecord = Loan(title, borrower, duration)
+
+    println("\n--- Detail Peminjaman ---")
+    println("Judul Buku  : ${loanRecord.bookTitle}")
+    println("Peminjam    : ${loanRecord.borrower}")
+    println("Lama Pinjam : ${loanRecord.loanDuration} hari")
+    println("Total Denda : Rp ${loanRecord.calculateFine()}")
 }
