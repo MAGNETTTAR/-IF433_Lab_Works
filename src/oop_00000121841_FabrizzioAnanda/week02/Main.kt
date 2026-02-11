8cab18ecf67cd87008ff1f1c29c4a4b609e35e54
FabrizzioAnanda <fabrizzio.ananda@student.umn.ac.id>
2026-02-11 15:40:03 +0700

    week02: implement user input and pre-validation

--- src/oop_00000121841_FabrizzioAnanda/week02/Main.kt
+++ src/oop_00000121841_FabrizzioAnanda/week02/Main.kt
@@ -1,5 +1,21 @@
-package oop_00000121841_FabrizzioAnanda.week02
+package oop_001_johnthor.week02
+
 import java.util.Scanner
+
+class Student (val name: String, val nim: String, var major: String) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+}
 
 fun main() {
     val scanner = Scanner(System.`in`)
