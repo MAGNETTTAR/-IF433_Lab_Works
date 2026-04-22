fab68d6e35b05bd53430d61a56c219386e9e7b48
FabrizzioAnanda <fabrizzio.ananda@student.umn.ac.id>
2026-04-22 15:55:11 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000121841_FabrizzioAnanda/week09/TradingDashboard.kt
+++ src/oop_00000121841_FabrizzioAnanda/week09/TradingDashboard.kt
@@ -21,4 +21,10 @@ fun main() {
     .map { "LOSS [${it.pair} ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 
     val uniquePairs = tradeHistory.map { it.pair }.toSet()
+
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
+    worstPerformersString.forEach { println(it) }
+
+    println("\nUnique Pairs: $uniquePairs")
 }
