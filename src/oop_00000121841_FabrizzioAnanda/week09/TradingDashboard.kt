a524eb67aeb8e92a123e2be7a09d7fedf066d47c
FabrizzioAnanda <fabrizzio.ananda@student.umn.ac.id>
2026-04-22 15:49:12 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000121841_FabrizzioAnanda/week09/TradingDashboard.kt
@@ -0,0 +1,11 @@
+package oop_00000121841_FabrizzioAnanda.week09
+
+fun main() {
+    val tradeHistory = listOf(
+    TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+    TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+    TradeLog("SOLUSDT", "LONG", 5, 12.0, "OPEN"),
+    TradeLog("BTCUSDT", "SHORT", 25, -2.5, "CLOSED"),
+    TradeLog("BNBUSDT", "LONG", 15, 8.4, "CLOSED"),
+    TradeLog("ETHUSDT", "LONG", 10, -10.0, "CLOSED")
+    )}
